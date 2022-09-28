168f1a6ff602f6953090e6f77ae5dee41bc54122
DjGiakma94 <davidedigiacomo94@gmail.com>
2022-09-28 09:56:15 +0200

    try microservice

---
+++ metadataMicroservice.js
@@ -0,0 +1,12 @@
+//You can submit a form that includes a file upload.
+
+//The form file input field has the name attribute set to upfile.
+
+//When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
+
+const express = require('express')
+const app = express()
+
+console.log("try");
+
+app.listen(3000)
